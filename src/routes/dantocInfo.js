0026ab2c6e3f2e4910f099f27a130dbdacdf986a
duynhandev0511 <tranduynhan497@gmail.com>
2025-02-03 20:27:47 +0700

    upgrade css Infodantoc

--- src/routes/dantocInfo.js
+++ src/routes/dantocInfo.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../stylesheet/54dantocInfo.css';
+import { Link } from 'react-router-dom';
 
 const EthnicGroupInfo = ({ groupInfo }) => {
+  const [zoomedImage, setZoomedImage] = useState(null);
+
   const {
     name,
     selfName,
@@ -20,14 +23,17 @@ const EthnicGroupInfo = ({ groupInfo }) => {
 
   const imgPath = process.env.PUBLIC_URL + '/ethnic/';
 
+  const handleImageZoom = (imageSrc) => {
+    setZoomedImage(imageSrc);
+  };
+
   return (
     <div className="ethnic-info-container">
-      {/* Back button */}
       <div className="container py-3">
         <div className="back-link">
-          <a href="#" className="text-decoration-none">
-            <span className="text-muted">← Back</span>
-          </a>
+          <Link to="/ethnic-groups" className="text-decoration-none">
+            <span className="text-muted">← Quay lại</span>
+          </Link>
         </div>
       </div>
 
@@ -37,20 +43,50 @@ const EthnicGroupInfo = ({ groupInfo }) => {
             {/* Left side - Avatar Images */}
             <div className="col-lg-5">
               <div className="avatar-group">
-                <div className="avatar-circle primary">
+                <div className="image-container avatar-circle primary">
                   <img
                     src={imgPath + image1}
-                    alt={`${name} - Primary`}
-                    className="img-fluid"
+                    alt={`${name} - Hình chính`}
+                    className="img-fluid zoomable"
+                    onClick={() => handleImageZoom(imgPath + image1)}
                   />
+                  <div 
+                    className="zoom-overlay" 
+                    onClick={() => handleImageZoom(imgPath + image1)}
+                  >
+                    <svg 
+                      xmlns="http://www.w3.org/2000/svg" 
+                      fill="white" 
+                      viewBox="0 0 24 24" 
+                      className="zoom-icon"
+                    >
+                      <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                      <path d="M12 10h-2v2H9v-2H7V9h2V7h1v2h2v1z"/>
+                    </svg>
+                  </div>
                 </div>
                 {image2 && (
-                  <div className="avatar-circle secondary">
+                  <div className="image-container avatar-circle secondary">
                     <img
                       src={imgPath + image2}
-                      alt={`${name} - Secondary`}
-                      className="img-fluid"
+                      alt={`${name} - Hình phụ`}
+                      className="img-fluid zoomable"
+                      onClick={() => handleImageZoom(imgPath + image2)}
                     />
+                    <div 
+                      className="zoom-overlay" 
+                      onClick={() => handleImageZoom(imgPath + image2)}
+                    >
+                      <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        fill="white" 
+                        viewBox="0 0 24 24" 
+                        className="zoom-icon"
+                      >
+                        <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                        <path d="M12 10h-2v2H9v-2H7V9h2V7h1v2h2v1z"/>
+                      </svg>
+                    </div>
                   </div>
                 )}
                 <div className="ethnic-name">
